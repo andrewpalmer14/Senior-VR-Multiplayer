4be35805d56d9d85f69d4e802e174a80d406ceab
andrewpalmer14 <andrewpalmer@u.boisestate.edu>
2018-12-11 12:42:01 -0700

    Multiplayer UI Fix. Multiplayer Quest Added. Single Player Quest 1 UI Fixed
    Lots of good stuff fixed!

---
+++ Project Atonement/Assets/HideUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class HideUI : MonoBehaviour {
+
+    private bool isHidden = false;
+    public GameObject uiCanvasObject;
+    private float delay = 0.2f;
+    private float time = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (CrossPlatformInputManager.GetButton("any"))
+        {
+            if (time > delay)
+            {
+                time = 0;
+                this.isHidden = !isHidden;
+                this.uiCanvasObject.SetActive(isHidden);
+            }
+        }
+	}
+}
